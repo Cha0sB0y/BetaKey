e2ce9335a49e1e24edafe5c1feceebe8ebb3feb5
luuuuuis <kawejh3296adzgja23t7asdghj>
2018-12-12 14:26:39 +0100

    Main

---
+++ src/main/java/de/luuuuuis/BetaKey.java
@@ -0,0 +1,13 @@
+package de.luuuuuis;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BetaKey extends Plugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+        System.out.println("yee");
+    }
+}
